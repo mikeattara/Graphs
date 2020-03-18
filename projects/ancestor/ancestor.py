f4d2a126ad3a932e929ee91511734cf0f4b4b60a
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-03-18 07:50:10 +0000

    create dfs helper function

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,23 @@
+from collections import defaultdict
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
+
+
+def dfs(graph, starting_node, ancestors):
+    visited = set()
+    stack = []
+    stack.append([starting_node])
+    while len(stack) > 0:
+        path = stack.pop()
+        nodes = set(path) - visited
+        for vertex in nodes:
+            parents = [parent for (parent, child)
+                       in ancestors if child == vertex]
+            if len(parents) == 0:
+                return -1 if path[-1] == starting_node else path[-1]
+            new_path = list(path)
+            new_path.append(min(parents))
+            stack.append(new_path)
+            visited.add(vertex)
