5e7d8ee0a4d3f41eee41431bd4d9d9542682ae9f
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-03-19 08:44:50 +0000

    refactor

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,21 @@
 import random
-from util import Queue
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
 
 
 class User:
