c264d21f131a1c63b78b9182b185a4095844b852
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-03-19 17:20:34 +0000

    complete

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,24 +5,6 @@ from world import World
 import random
 from ast import literal_eval
 
-
-class Queue():
-    def __init__(self):
-        self.queue = []
-
-    def enqueue(self, value):
-        self.queue.append(value)
-
-    def dequeue(self):
-        if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
-
-    def size(self):
-        return len(self.queue)
-
-
 # Load world
 world = World()
 
@@ -44,116 +26,44 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+# need reverse directions because they are opposite based on what appears on map visual
+reverse_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
 
-def dft(room, visited, random_direction):
-    # dft
-    stack = []
-    stack.append(player.current_room.id)
-    is_dead_end = False
+def traverse(room, visited=None):
+    if visited is None:
+        visited = set()
+    traversed_path = []
 
-    while len(stack) > 0:
-        v = stack.pop()
-        if v not in visited:
-            if player.current_room.id not in visited:
+    # add starting room to visited set
+    visited.add(room.id)
 
-                # log directions with '?'
-                visited[v] = {}
-                for i in range(0, len(player.current_room.get_exits())):
-                    visited[v][player.current_room.get_exits()[i]] = '?'
+    # for each direction provided in room exits method
+    for direction in room.get_exits():
+        # move to that room and store in next_room
+        next_room = room.get_room_in_direction(direction)
 
-                if random_direction in player.current_room.get_exits():
-                    prev = player.current_room.id
-                    # travels
-                    player.travel(random_direction)
+        # if that room has not already been visited...
+        if next_room.id not in visited:
+            # recurse on that room
+            next_room_path = traverse(next_room, visited)
 
-                    # logs the direction
-                    visited[v][random_direction] = player.current_room.id
-                    traversal_path.append(random_direction)
-                else:
-                    is_dead_end = True
+            # after recursing, if valid direction path, store path
+            if next_room_path:
+                path = [direction] + next_room_path + \
+                    [reverse_directions[direction]]
+            # not valid path so reverse direction and store that path
+            else:
+                path = [direction, reverse_directions[direction]]
+            # add path taken to traversed array
+            traversed_path = traversed_path + path
 
-                if random_direction == 'n':
-                    visited[v]['s'] = prev
-                if random_direction == 's':
-                    visited[v]['n'] = prev
-                if random_direction == 'e':
-                    visited[v]['w'] = prev
-                if random_direction == 'w':
-                    visited[v]['e'] = prev
-
-            for neighbor in room[v][1]:
-                next = room[v][1][neighbor]
-                stack.append(next)
-    print(is_dead_end)
-    # return is_dead_end
+    # return complete path to traversing the maze
+    return traversed_path
 
 
-def traverse_path(room):
-    """
-    picks a random unexplored direction from the player's current room
-    """
-    # previous node
-    prev = player.current_room.id
-    # create traverseal graph
-    visited = {}
-    visited[0] = {}
-    for i in range(0, len(player.current_room.get_exits())):
-        visited[0][player.current_room.get_exits()[i]] = '?'
+traversal_path = traverse(player.current_room)
 
-    random_direction = None
-    # pick random unexplosed direction - picks next one on the list
-    for direction in visited[player.current_room.id]:
-        if visited[0][direction] == '?':
-            random_direction = direction
-            break
-
-    """
-    travels and logs that direction
-    """
-
-    # travels
-    player.travel(random_direction)
-
-    # logs the direction
-    visited[0][random_direction] = player.current_room.id
-    traversal_path.append(random_direction)
-
-    """
-    loops
-    """
-
-    # while len(visited) != len(room):
-
-    # dft(room, visited, random_direction)
-
-    # bfs
-    visited = set()
-    queue = Queue()
-    queue.enqueue([player.current_room.id])
-    while queue.size() > 0:
-        path = queue.dequeue()
-        v = path[-1]
-        if v not in visited:
-            visited.add(v)
-
-            for exit in g[v]:
-                if g[v][exit] == '?':
-                    return path
-            for neighbor in room[v][1]:
-                new_path = [*path, neighbor]
-                queue.enqueue(new_path)
-
-
-g = {}
-
-while len(g) < len(room_graph):
-    current = player.current_room.id
-    print(current)
-    if current not in g:
-        g[current] = current
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -175,12 +85,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
